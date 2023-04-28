d6333320db1e7e7f333e98a0fd7fbc229755ed59
K-MOHANAKRISHNA <85103363+K-MOHANAKRISHNA@users.noreply.github.com>
2023-04-28 08:03:41 +0530

    Update 2a.c

--- 2a.c
+++ 2a.c
@@ -30,3 +30,32 @@ output:-
 hello world 
 ???? ??????
 
+import java.util.*;
+import java.io.*;
+public class SubstitutionCipher
+{
+    static Scanner sc=new Scanner(System.in);
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String args[])throws IOException
+    {
+        String a="abcdefghijklmnopqrstuvwxyz";
+        String b="zyxwvutsrqponmlkjihgfedcba";
+        System.out.println("Enter any string:");
+        String str=br.readLine();
+        String decrypt="";
+        char c;
+        for(int i=0;i<str.length();i++)
+        {
+            c=str.charAt(i);
+            int j=a.indexOf(c);
+            decrypt=decrypt+b.charAt(j);
+        }
+        System.out.println("The encrypted data is :"+decrypt);
+    }
+}
+
+
+
+Enter any string:
+helloworld
+The encrypted data is :svooldliow
